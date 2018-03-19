006e64e3d9e9f8706da1b25f058bf260a8b03d73
Mathewe Banda <bbanda@oberlin.edu>
2018-03-19 00:32:27 -0400

    Squares work! Average pixel brightness is a reality! And it's generic! WooHoo!

--- twoColorImage/twoColor.py
+++ twoColorImage/twoColor.py
@@ -2,6 +2,8 @@ from PIL import Image
 import numpy as np
 import math
 
+tileSize = 13 #defines the square size of a given tile
+
 class Square:
     def __init__(self):
         self.c = None #0 if White, 1 if Black. Determines direction of slider
@@ -27,32 +29,60 @@ class Grid:
     def __init__(self):
         self.squares = [] #2D array of all squares on the grid in l-r u-d order
         self.photo = None
+        self.dimensions = [] #x,y dimensions of the photo
         
+    def getDimensions(self):
+        self.dimensions.append(len(self.photo[0])) #x
+        self.dimensions.append(len(self.photo)) #y
+        print(self.dimensions)
 
-def makeSquares(imgArray):
-    return None    
+def makeDimensionsWork(img):
+    x = img.size[0]
+    y = img.size[1]
+    
+    while x%tileSize != 0:
+        x += 1
+    while y%tileSize != 0: 
+        y += 1
+        
+    img = img.resize([x,y], Image.ANTIALIAS)
+    return img
+    
 
-def resizeImage (img, xyDims): 
-    for instanceType in instances:
-        for instance in instanceType:
-            instance.image = instance.image.resize(size, Image.ANTIALIAS)
+def makeSquares(grid):
+    squareNum = 0
+    #print(grid.dimensions[1]//tileSize, grid.dimensions[0]//tileSize)
+    for y in range(0, len(grid.photo)//tileSize):
+        grid.squares.append([]) #makes a new level in 2D array
+        for x in range(0, len(grid.photo[0])//tileSize):
+            for a in range(0,tileSize):
+                for b in range(0,tileSize):
+                    squareNum += grid.photo[y*tileSize + a][x*tileSize + b]
+                    
+            grid.squares[y].append(int(squareNum//(tileSize*tileSize)))
+            squareNum = 0
+    #print(grid.squares)
             
 def numPyIfy(img): #creates a 2D array of pixels
     img = np.asarray(img, dtype=np.float32)
     return img
             
 def makeImage(imageName):
-    img = Image.open(imageName).convert('L') #image must be in same folder
+    img = Image.open(imageName).convert('L') #Black & White
     storedImg = img.copy()
     img.close()
+
+    storedImg = makeDimensionsWork(storedImg)
     storedImg = numPyIfy(storedImg)
+
     return storedImg
 
 def main():
-    img = makeImage("monaLisa.jpg")
     grid = Grid()
-    grid.photo = img
-    print(grid.photo)
-    grid.squares = makeSquares(grid.photo)
+    grid.photo = makeImage("monaLisa.jpg")
+    print(len(grid.photo), len(grid.photo[0]))
+    makeSquares(grid)
+    print(grid.squares)
+    
     
 main()
